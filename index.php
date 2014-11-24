3885afb827b4b2d90c75dcfa322e267ed751ea15
blha303 <stevensmith.ome@gmail.com>
2014-11-24 12:59:24 -0500

    Revert "Updated CSS. Now actually looks decent."
    This reverts commit 92f3de41e1c8ced59b9d45f6784e42a0122398fe.

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
-<html>
-  <head>
-    <link rel="stylesheet" type="text/css" href="main.css">
-  </head>
-  <body>
-<!--    <a href="https://github.com/blha303/mcsanta">
-      <img alt="Fork me on GitHub" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" style="position: absolute; top: 0; right: 0; border: 0;">
-    </a> -->
-    <div id="container">
-      <h1>Minecraft Hatomator<sup><sup><a target="_blank" style="font-size: 5pt; text-decoration: none; color: black" href="http://www.reddit.com/r/Minecraft/comments/1rwuwo/i_made_a_site_to_easily_add_a_santa_hat_to_your/cds793d">[x]</a></sup></sup></h1>
 <?php
 $siteloc = "http://blha303.com.au/mcsanta";
 
@@ -30,10 +18,6 @@ if($httpCode != 200) {
     die();
 }
 curl_close($handle);
-if (isset($_GET['refresh'])) {
-  unlink(realpath(dirname(__FILE__)) ."/tmp/$user-santa.png");
-  unlink(realpath(dirname(__FILE__)) ."/tmp/$user.png");
-}
 if (!file_exists(realpath(dirname(__FILE__)) ."/tmp/$user-santa.png")) {
 $skinimage = file_get_contents("https://s3.amazonaws.com/MinecraftSkins/$user.png");
 $fp = fopen(realpath(dirname(__FILE__)) ."/tmp/$user.png", "w");
@@ -48,27 +32,18 @@ imageSaveAlpha($userskin, true);
 imagecopy($userskin, $santatemplate, 0, 0, 0, 0, imagesx($userskin), imagesy($userskin));
 imagepng($userskin, realpath(dirname(__FILE__)) ."/tmp/$user-santa.png");
 } ?>
-<p id="applyp"><a id="applylink" href="https://minecraft.net/profile/skin/remote?url=<?php echo $siteloc."/tmp/".$user; ?>-santa.png">Click here to apply your santa-hatted skin!</a><br>(Make sure to <a href="https://s3.amazonaws.com/MinecraftSkins/<?php echo $user; ?>.png" id="backuplink">click here first</a> to download your current skin, or you won't be able to get it back.)</p>
-<div id="frame"><iframe src="skin/?user=<?php echo $user; ?>" title="skin" width="200px" height="200px" id="preview"></iframe>
-<br><small>Skin preview by <a href="http://forums.bukkit.org/threads/web-html5-skin-viewer.4428/">earthiverse</a></small></div>
-<!-- <p id='disclaimer'>This model is not completely accurate, the sides of the head are reversed. The skin applied to your character doesn't have this problem. We're working on a fix; thanks for your patience :)</p> -->
-<p id="backtohomepage"><a href="<?php echo $siteloc; ?>">Back to homepage</a></p>
+<a href="https://minecraft.net/profile/skin/remote?url=<?php echo $siteloc."/tmp/".$user; ?>-santa.png">Click here to apply your santa-hatted skin!</a><br>(Make sure to <a href="https://s3.amazonaws.com/MinecraftSkins/<?php echo $user; ?>.png">click here first</a> to download your current skin, or you won't be able to get it back.)<br>
+<iframe src="skin/?user=<?php echo $user; ?>" title="skin" width="200px" height="200px"></iframe>
+<br><small>Skin preview by <a href="http://forums.bukkit.org/threads/web-html5-skin-viewer.4428/">earthiverse</a></small>
+<br><a href="<?php echo $siteloc; ?>">Back to homepage</a>
 <?php } else { ?>
+<h1>Minecraft skin santa hat automater</h1>
 <form method="GET">
-<label for="user">Username</label>
-<input id="user" type="text" name="user"><input type="submit" id="submit">
+<input type="text" name="user"><input type="submit">
 </form>
 <?php if (isset($_GET['error'])) {
-  echo '<div id="error">';
   if ($_GET['error'] == "char") { echo "Invalid characters provided."; }
   else if ($_GET['error'] == "haspaid") { echo "No Minecraft skin found for that username."; }
-  echo '</div>';
 } ?>
-<p id="maynotwork">May not work properly if your skin doesn't have a transparent background, as the santa hat is overlayed over your existing skin. Tweet at <a href="https://twitter.com/blha303">@blha303</a> or join <a href="http://webchat.esper.net/?channels=#mcsanta">irc.esper.net #mcsanta</a> (<a href="irc://irc.esper.net/mcsanta">irc://</a>) for help.</p>
-<?php } ?>
-<p id="skincount"><b>Generated skins:</b> <?php include('tmp/count.php'); ?></p>
-<a id="twitter" data-url="http://blha303.com.au/mcsanta" data-text="Check out this cool webapp for applying a Santa hat to your Minecraft skin!" href="https://twitter.com/share" data-related="blha303" data-via="blha303" class="twitter-share-button" data-lang="en" data-size="large">Tweet</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-</div>
-</body>
-</html>
+<p>May not work properly if your skin doesn't have a transparent background, as the santa hat is overlayed over your existing skin. Tweet at <a href="https://twitter.com/blha303">@blha303</a> or join <a href="irc://irc.esper.net/mcsanta">irc.esper.net #mcsanta</a> for help.
+<?php }
